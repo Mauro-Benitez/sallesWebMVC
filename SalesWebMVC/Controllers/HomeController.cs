a99d7a08897497198c84ddf4aaad262ef6cf4c56
Mauro-Benitez <mauro.cbenitez@gmail.com>
2024-01-24 12:02:38 -0300

    Primeiro Model-View-Controller de Departament

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+using SalesWebMVC.Models.ViewModels; 
 namespace SalesWebMVC.Controllers
 {
     public class HomeController : Controller
@@ -15,10 +15,11 @@ namespace SalesWebMVC.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult About() 
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web";
             ViewData["Email"] = "mauro@email.com";
+            ViewData["Senha"] = "*****";
 
             return View();
         }
