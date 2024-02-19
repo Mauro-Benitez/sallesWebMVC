fb7f4c124f132ff1bc37306c881353322e1f0fee
Mauro-Benitez <mauro.cbenitez@gmail.com>
2024-02-18 21:22:32 -0300

    Create SellerService and basic FindAll

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 using SalesWebMVC.Data;
-
+using SalesWebMVC.Services;
 namespace SalesWebMVC
 {
     public class Startup
@@ -42,6 +42,7 @@ namespace SalesWebMVC
                         builder.MigrationsAssembly("SalesWebMVC")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
 
 
         }
